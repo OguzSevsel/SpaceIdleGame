1e10afc48eca6959aa747cf2220b19cdbd02fd4c
Oğuz Sevsel <oguzsevsel@gmail.com>
2025-09-25 17:28:53 +0300

    Feat (UI): Added TabSystems

---
+++ Assets/Scripts/UI/TabButton.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(Image))]
+public class TabButton : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private TabGroup tabGroup;
+    public Image backgroundImage;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        tabGroup.OnTabSelected(this);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        tabGroup.OnTabEnter(this);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        tabGroup.OnTabExit(this);
+    }
+
+    private void Start()
+    {
+        backgroundImage = GetComponent<Image>();
+        tabGroup.Subscribe(this);
+    }
+}
---
+++ Assets/Scripts/UI/TabGroup.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TabGroup : MonoBehaviour
+{
+    private List<TabButton> TabButtons;
+    [SerializeField] private Sprite tabIdle;
+    [SerializeField] private Sprite tabHover;
+    [SerializeField] private Sprite tabSelected;
+    private TabButton selectedTab;
+    public List<GameObject> objectsToSwap;
+
+    public void Subscribe(TabButton button)
+    {
+        if (TabButtons == null)
+        {
+            TabButtons = new List<TabButton>();
+        }
+
+        TabButtons.Add(button);
+    }
+
+    public void OnTabEnter(TabButton button)
+    {
+        ResetTabs();
+        if (selectedTab == null || button != selectedTab)
+        {
+            button.backgroundImage.sprite = tabHover;
+        }
+    }
+
+    public void OnTabExit(TabButton button)
+    {
+        ResetTabs();
+
+    }
+
+    public void OnTabSelected(TabButton button)
+    {
+        selectedTab = button;
+        ResetTabs();
+        button.backgroundImage.sprite = tabSelected;
+
+        int index = button.transform.GetSiblingIndex();
+
+        for (int i = 0; i < objectsToSwap.Count; i++)
+        {
+            if (i == index)
+            {
+                objectsToSwap[i].SetActive(true);
+            }
+            else
+            {
+                objectsToSwap[i].SetActive(false);
+            }
+        }
+    }
+
+    public void ResetTabs()
+    {
+        foreach (var button in TabButtons)
+        {
+            if (selectedTab != null && button == selectedTab)
+            {
+                continue;
+            }
+            button.backgroundImage.sprite = tabIdle;
+        }
+    }
+}
